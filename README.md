78274ba94726db9486e356b6cb10458e0655a90b
robfairh <ref3@illinois.edu>
2020-07-21 08:07:02 -0500

    Revert "Merge branch 'assembly' into compact"
    This reverts commit b4aa9f9928b63e799557c093a237a3b771227242, reversing
    changes made to c2bfee05fb1cab28eec8c3068964762aef5bd0c5.

--- README.md
+++ README.md
@@ -6,7 +6,3 @@ Also, the model in Serpent will be useful for comparing the results between Serp
 
 This repository holds the following serpent input files:
 * compact: serpent model of the fuel compact.
-* particles4.inp: defines the particle locations in the fuel compact.
-* particles79.inp: defines the particle locations in the fuel assembly.
-* rsc-fuel: model of rsc fuel assembly.
-* standard fuel: model of the standard fuel assembly.
--- auxiliary.py
+++ auxiliary.py
@@ -29,24 +29,8 @@ def compact():
     plt.savefig("compact", dpi=300, bbox_inches="tight")
 
 
-def standard():
-    '''
-    Adds legend to standard fuel assembly geometry image:
-    'standard-fuel_geom1.png'.
-    '''
-    cwd = os.getcwd()
-    fname = get_sample_data('%s/standard-fuel_geom1.png' % (cwd))
-    im = plt.imread(fname)
-    plt.imshow(im)
-    plt.legend(handles=[matrix, block, helium])
-
-    plt.axis('off')
-    plt.savefig("standard", dpi=300, bbox_inches="tight")
-
-
 def main():
     compact()
-    standard()
 
 
 if __name__ == "__main__":
