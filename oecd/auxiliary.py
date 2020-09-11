029ccebaa8a9fb2cf5dfcecc786c0a21e63a4005
robfairh <ref3@illinois.edu>
2020-09-11 15:33:29 -0500

    added figures for 600 and 1200K serpent fullcore models

--- oecd/auxiliary.py
+++ oecd/auxiliary.py
@@ -114,9 +114,65 @@ def fullcore_detectors():
     plt.savefig("oecd-fullcore-detectors", dpi=300, bbox_inches="tight")
 
 
+def fullcore_detectors_thesis():
+    '''
+    Adds legend to fuel core geometry image:
+    'oecd-fullcore_geom1.png'.
+    '''
+
+    cwd = os.getcwd()
+    fname = get_sample_data('%s/oecd-fullcore_geom1.png' % (cwd))
+    im = plt.imread(fname)
+
+    # crop the image
+    height, width, color = np.shape(im)
+    y1 = int(height*0.15)
+    y2 = int(height*0.6)
+    x1 = int(width*0.45)
+    x2 = int(width)
+    plt.imshow(im[y1:y2,x1:x2,:])
+    plt.axis('off')
+
+    # Axial 1
+    x = 158
+    y = 291
+    P = 55
+    s = P/2/np.cos(np.pi/6)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5, label='1- Axial 1')
+    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
+    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.text(x=x+35, y=y+42, s='1', fontsize=24, color='w')
+
+    # Axial 2
+    x = 262
+    y = 291
+    P = 55
+    s = P/2/np.cos(np.pi/6)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5, label='2- Axial 2')
+    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
+    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+
+    plt.text(x=x+35, y=y+42, s='2', fontsize=24, color='w')
+
+    # Radial 1
+    x = 52
+    y = 349
+    plt.plot([x, 495+x], [y, y], 'r-', lw=1.5, label='3- Radial')
+    plt.text(x=x+380, y=y-15, s='3', fontsize=24, color='black')
+
+    plt.legend(loc='best')
+    plt.savefig("oecd-fullcore-detectorsB", dpi=300, bbox_inches="tight")
+
+
 def main():
     # fullcore()
-    fullcore_detectors()
+    fullcore_detectors_thesis()
 
 
 if __name__ == "__main__":
--- plotter.py
+++ plotter.py
@@ -1,8 +1,12 @@
-# import os
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import serpentTools as st
-# from matplotlib.cbook import get_sample_data
+from matplotlib.cbook import get_sample_data
+from matplotlib.patches import RegularPolygon
+from matplotlib.collections import PatchCollection
+from matplotlib.pyplot import gca
+from matplotlib.axes import Axes
 
 
 def plot_spectrum(data, name, fig):
@@ -26,8 +30,9 @@ def plot_spectrum(data, name, fig):
     inte = sum(val*dE)
     val = val/inte
 
-    plt.figure()
-    plt.loglog(E, val)
+    # plt.figure()
+    plt.plot(E, val)
+    plt.xscale('log')
     plt.xlabel('E [MeV]')
     plt.ylabel('Normalized flux')
     plt.grid(True)
@@ -162,7 +167,7 @@ def int_flux(data, name):
     print('Int_G2: ', sum(val0))
 
 
-def plot_fullcore():
+def plot_old_fullcore():
     '''
     Plots the output of the detectors of the full core model.
     - Spectrum
@@ -219,5 +224,222 @@ def plot_column():
     plot_axial(data, 'Axial', 'column-LBP-' + str(G) + 'G-1200', V)
 
 
+def plot_serpent_axial_collapse(data, name, save, lim, V=1, dire='Z'):
+    """
+    Plots axial flux from serpent detector.
+    Collapses fluxes from G number of groups to Gp number of groups.
+
+    Parameters:
+    -----------
+    data: [serpenttools format]
+    name: [string]
+        name of the detector
+    save: [string]
+        name of the figure
+    lim: [list of int]
+        if lim = [2, 4, 6]:
+            - groups1 and groups2 form the new group1.
+            - groups3 and groups4 form the new group2.
+            - groups5 and groups6 form the new group3.
+        lim[-1] = G
+        len(lim) = Gp
+    V: [float]
+        total volume where the detector is applied [cm3]
+    dire: [float]
+        direction that the detector faces: 'X', 'Y', 'Z'
+    """
+
+    det = data.detectors[name]
+    z = [line[0] for line in det.grids[dire]]
+    val = det.tallies
+    vdetector = V/len(z)
+    val = val/vdetector
+
+    G = len(lim)
+
+    fluxes = np.zeros((G, len(val[0])))
+    for g in range(G):
+        if g == 0:
+            for i in range(lim[0]):
+                fluxes[g] += val[25-i]
+        else:
+            for i in range(lim[g-1], lim[g]):
+                fluxes[g] += val[25-i]
+
+    plt.figure()
+    for i in range(G):
+        plt.step(z, fluxes[i], where='post', label='g={0}'.format(i+1))
+
+    plt.xlabel(dire.lower()+' [cm]')
+    plt.ylabel(r'$\phi \left[\frac{n}{cm^2s}\right]$')
+    if G < 20:
+        plt.legend(loc="upper left", bbox_to_anchor=(1., 1.), fancybox=True)
+    else:
+        plt.legend(loc="upper left", bbox_to_anchor=(1., 1.2), fancybox=True)
+    plt.savefig(save, dpi=300, bbox_inches="tight")
+
+
+def plot_serpent_radial_collapse(data, name, save, lim, piH=1):
+    """
+    Plots flux from curvilinear detector.
+
+    Parameters:
+    -----------
+    data: [serpenttools format]
+    name: [string]
+        name of the detector
+    save: [string]
+        name of the figure
+    lim: 
+    piH: [float]
+        angle * total height of the detector [cm]
+    """
+    det = data.detectors[name]
+    r = np.array([line[0] for line in det.grids['R']])
+    vdetector = np.roll(r, -1)**2-r**2
+    vdetector[-1] = det.grids['R'][-1][1]**2 - det.grids['R'][-1][0]**2
+    vdetector *= piH/2
+    val = det.tallies
+    val = val/vdetector
+
+    G = len(lim)
+
+    fluxes = np.zeros((G, len(val[0])))
+    for g in range(G):
+        if g == 0:
+            for i in range(lim[0]):
+                fluxes[g] += val[25-i]
+        else:
+            for i in range(lim[g-1], lim[g]):
+                fluxes[g] += val[25-i]
+
+    plt.figure()
+
+    for i in range(G):
+        plt.step(r, fluxes[i], where='post', label='g={0}'.format(i+1))
+
+    plt.xlabel('r [cm]')
+    plt.ylabel(r'$\phi \left[\frac{n}{cm^2s}\right]$')
+    if G < 20:
+        plt.legend(loc="upper left", bbox_to_anchor=(1., 1.), fancybox=True)
+    else:
+        plt.legend(loc="upper left", bbox_to_anchor=(1., 1.2), fancybox=True)
+    plt.savefig(save, dpi=300, bbox_inches="tight")
+
+
+def plot_fullcore():
+    '''
+    Plots the output of the detectors of the full core model.
+    - Spectrum
+    - 3 axial flux detectors
+    - 3 radial flux detector
+    - Pin power distribution
+    '''
+    data = st.read('oecd-fullcore26G_det1b1.m', reader='det')
+    lim26 = [4, 16, 26]  # from 26 to 3
+    A = 18/np.cos(np.pi/6)  # cm length of face of the hexagon
+    Ah = 6. * (A * 18./2)   # Area of the hexagon
+    V = Ah * (160 + 793 + 120)
+    H = 79.3
+    p = np.pi/180 * 2  # = 2 deg
+    save = 'serpent26G-600-collapse-Axial1'
+    plot_serpent_axial_collapse(data, 'Axial1', save, lim26, V, 'Z')
+    save = 'serpent26G-600-collapse-Axial2'
+    plot_serpent_axial_collapse(data, 'Axial3', save, lim26, V, 'Z')
+    save = 'serpent26G-600-collapse-Radial'
+    plot_serpent_radial_collapse(data, 'Radial3', save, lim26, p*H)
+
+
+def plot_radial_power_distribution():
+    '''
+    Plots the power distribution of a 1/6th of the MHTGR350.
+    The position of the fuel assemblies has to be introduced into
+    the array 'coord' manually.
+
+    Parametres:
+    -----------
+    file: [string]
+        name of the .csv file
+    save: [string]
+        name of the figure to produce
+    '''
+
+    data = st.read('oecd-fullcore26G-1200_det1b1.m', reader='det')
+    save = 'serpent26G-1200-power'
+    det = data.detectors['power']
+
+    power = np.zeros(11)
+    power[0] = det.tallies[0, 3]
+    power[1] = det.tallies[0, 4]
+    power[2] = det.tallies[1, 2]
+    power[3] = det.tallies[1, 3]
+    power[4] = det.tallies[1, 4]
+    power[5] = det.tallies[2, 1]
+    power[6] = det.tallies[2, 2]
+    power[7] = det.tallies[2, 3]
+    power[8] = det.tallies[3, 1]
+    power[9] = det.tallies[3, 2]
+    power[10] = det.tallies[4, 1]
+    power /= 1e6  # W to MW
+
+    P = 36  # pitch
+    F = P / np.sqrt(3)
+    coord = []
+    # 1 - 2
+    coord.append(np.array([0, 3*P]))
+    coord.append(np.array([0, 4*P]))
+    # 3 - 5
+    coord.append(np.array([1*(F+F/2), 3*P-P/2]))
+    coord.append(np.array([1*(F+F/2), 4*P-P/2]))
+    coord.append(np.array([1*(F+F/2), 5*P-P/2]))
+    # 6 - 8
+    coord.append(np.array([2*(F+F/2), 2*P]))
+    coord.append(np.array([2*(F+F/2), 3*P]))
+    coord.append(np.array([2*(F+F/2), 4*P]))
+    # 9 - 10
+    coord.append(np.array([3*(F+F/2), 3*P-P/2]))
+    coord.append(np.array([3*(F+F/2), 4*P-P/2]))
+    # 11
+    coord.append(np.array([4*(F+F/2), 3*P]))
+    coord = np.array(coord)
+
+    patches = []
+    xmax, ymax = [-np.inf, ] * 2
+    xmin, ymin = [np.inf, ] * 2
+    for i in range(len(coord)):
+        h = RegularPolygon(coord[i], 6, F, np.pi/2)
+        patches.append(h)
+        verts = h.get_verts()
+        vmins = verts.min(0)
+        vmaxs = verts.max(0)
+        xmax = max(xmax, vmaxs[0])
+        xmin = min(xmin, vmins[0])
+        ymax = max(ymax, vmaxs[1])
+        ymin = min(ymin, vmins[1])
+
+    patches = np.array(patches, dtype=object)
+    pc = PatchCollection(patches)
+
+    ax = gca()
+    pc.set_array(power)
+    ax.add_collection(pc)
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+
+    cbar = plt.colorbar(pc)
+    cbar.ax.set_ylabel('Power [MW]')
+
+    for i in range(11):
+        plt.text(x=coord[i][0]-F/3, y=coord[i][1]-2.5,
+                 s=np.round(power[i], 2), fontsize=12, color='w')
+
+    plt.axis('equal')
+    plt.xlabel('X [cm]')
+    plt.ylabel('Y [cm]')
+    plt.savefig(save, dpi=300, bbox_inches="tight")
+
+
+# plot_old_fullcore()
+# plot_column()
 # plot_fullcore()
-plot_column()
+# plot_radial_power_distribution()
