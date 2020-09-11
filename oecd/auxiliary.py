804678bf9c51a367a19cf5db5a9ceefbac851596
robfairh <ref3@illinois.edu>
2020-09-11 10:42:25 -0500

    added function that adds detector postion to the figure

--- oecd/auxiliary.py
+++ oecd/auxiliary.py
@@ -2,7 +2,7 @@ import os
 from matplotlib.cbook import get_sample_data
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def assembly():
     uco = mpatches.Patch(color=(1., 1., 0.), label='UCO')
@@ -36,8 +36,70 @@ def fullcore():
     plt.savefig("oecd-fullcore", dpi=300, bbox_inches="tight")
 
 
+def fullcore_detectors():
+    '''
+    Adds legend to fuel core geometry image:
+    'oecd-fullcore_geom1.png'.
+    '''
+
+    matrix = mpatches.Patch(color=(0.63, 0.63, 0.63),
+                            label='Graphite Fuel Block')
+    block = mpatches.Patch(color=(0.61, 1., 0.91),
+                           label='Graphite Reflector')
+
+    cwd = os.getcwd()
+    fname = get_sample_data('%s/oecd-fullcore_geom1.png' % (cwd))
+    im = plt.imread(fname)
+
+    # crop the image
+    height, width, color = np.shape(im)
+    y1 = int(height*0.15)
+    y2 = int(height*0.6)
+    x1 = int(width*0.45)
+    x2 = int(width)
+    plt.imshow(im[y1:y2,x1:x2,:])
+    plt.legend(handles=[matrix, block])
+
+    plt.axis('off')
+
+    x = 158
+    y = 291
+    P = 55
+    s = P/2/np.cos(np.pi/6)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5)
+    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
+    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+
+    x = 210
+    y = 321
+    P = 55
+    s = P/2/np.cos(np.pi/6)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5)
+    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
+    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+
+    x = 262
+    y = 291
+    P = 55
+    s = P/2/np.cos(np.pi/6)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5)
+    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
+    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+
+    plt.savefig("oecd-fullcore-detectors", dpi=300, bbox_inches="tight")
+
+
 def main():
-    fullcore()
+    fullcore_detectors()
 
 
 if __name__ == "__main__":
