0c536ee3dd4accbebc7e44d8b97924213e39bba4
robfairh <ref3@illinois.edu>
2020-09-11 12:42:39 -0500

    added detector positions to figure

--- oecd/auxiliary.py
+++ oecd/auxiliary.py
@@ -42,11 +42,6 @@ def fullcore_detectors():
     'oecd-fullcore_geom1.png'.
     '''
 
-    matrix = mpatches.Patch(color=(0.63, 0.63, 0.63),
-                            label='Graphite Fuel Block')
-    block = mpatches.Patch(color=(0.61, 1., 0.91),
-                           label='Graphite Reflector')
-
     cwd = os.getcwd()
     fname = get_sample_data('%s/oecd-fullcore_geom1.png' % (cwd))
     im = plt.imread(fname)
@@ -58,47 +53,69 @@ def fullcore_detectors():
     x1 = int(width*0.45)
     x2 = int(width)
     plt.imshow(im[y1:y2,x1:x2,:])
-    plt.legend(handles=[matrix, block])
-
     plt.axis('off')
 
+    # Axial 1
     x = 158
     y = 291
     P = 55
     s = P/2/np.cos(np.pi/6)
-    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5, label='1- Axial1')
     plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
     plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
     plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
     plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
     plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
 
+    plt.text(x=x+37, y=y+40, s='1', fontsize=20, color='w')
+
+    # Axial 2
     x = 210
     y = 321
     P = 55
     s = P/2/np.cos(np.pi/6)
-    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5, label='2- Axial2')
+    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
+    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
+    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.text(x=x+37, y=y+40, s='2', fontsize=20, color='w')
+
+    # Axial 3
+    x = 262
+    y = 291
+    P = 55
+    s = P/2/np.cos(np.pi/6)
+    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5, label='3- Axial3')
     plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
     plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
     plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
     plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
     plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
 
-    x = 262
-    y = 291
-    P = 55
-    s = P/2/np.cos(np.pi/6)
-    plt.plot([s+x, 2*s+x], [0+y, 0+y], 'r-', lw=1.5)
-    plt.plot([s+x, 2*s+x], [P+y, P+y], 'r-', lw=1.5)
-    plt.plot([s+x, s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
-    plt.plot([s/2+x, s+x], [P/2+y, P+y], 'r-', lw=1.5)
-    plt.plot([2*s+x, 2*s+s/2+x], [0+y, P/2+y], 'r-', lw=1.5)
-    plt.plot([2*s+s/2+x, 2*s+x], [P/2+y, P+y], 'r-', lw=1.5)
+    plt.text(x=x+37, y=y+40, s='3', fontsize=20, color='w')
+
+    # Radial 1
+    x = 52
+    y = 349
+    plt.plot([x, 495+x], [y, y], 'r-', lw=1.5, label='4- Radial1')
+    plt.plot([x, 495+x], [y, y], 'r-', lw=1.5, label='5- Radial2')
+    plt.text(x=x+380, y=y-10, s='4, 5', fontsize=20, color='black')
+
+    # Radial 2
+    x = 52
+    y = 349
+    L = 495
+    plt.plot([x, L*np.cos(np.pi/6)+x], [y, -L/2+y], 'r-', lw=1.5, label='6- Radial3')
+    plt.text(x=350, y=y-200, s='6', rotation=30, fontsize=20, color='black')
+    plt.legend(loc='best')
 
     plt.savefig("oecd-fullcore-detectors", dpi=300, bbox_inches="tight")
 
 
 def main():
+    # fullcore()
     fullcore_detectors()
 
 
