e03949c79377960bebece1c329c72d05587b3f53
robfairh <ref3@illinois.edu>
2020-09-12 11:30:03 -0500

    git fixed energy integrated spectrum

--- plotter.py
+++ plotter.py
@@ -47,43 +47,6 @@ def reagroup(E, val, E0):
     Parameters:
     -----------
     E: [array]
-    	contains energy group limits
-    val: [array]
-    	contains fluxes for each energy bins
-    E0: [array]
-    	contains new enegy group limist
-    '''
-
-    e = 1e-13
-    new_val = np.zeros(len(E0))
-    j = 0
-    for i in range(len(E)-1):
-        if j < len(E0)-2:
-            if E[i] >= E0[j]-e and E[i] < E0[j+1]+e:
-                new_val[j] += val[i]*(E[i+1]-E[i])/(E0[j+1]-E0[j])
-            else:
-                j += 1
-                continue
-        else:
-            if E[i] >= E0[j]-e:
-                new_val[j] += val[i]*(E[i+1]-E[i])/(E[-1]-E0[j])
-
-    # this adds an additional point in the very beginning
-    new_val = np.roll(new_val, 1)
-    new_val[0] = new_val[1]
-    E = E0
-    val = new_val
-    return E, val
-
-
-def reagroup2(E, val, E0):
-    '''
-    This function converts a fine energy grid spectrum into a
-    coarser one.
-
-    Parameters:
-    -----------
-    E: [array]
         contains energy group limits
     val: [array]
         contains fluxes for each energy bins
@@ -98,12 +61,14 @@ def reagroup2(E, val, E0):
         if j < len(E0)-2:
             if E[i] >= E0[j]-e and E[i] < E0[j+1]+e:
                 new_val[j] += val[i]
+                # new_val[j] += val[i]*(E[i+1]-E[i])/(E0[j+1]-E0[j])
             else:
                 j += 1
                 continue
         else:
             if E[i] >= E0[j]-e:
                 new_val[j] += val[i]
+                #new_val[j] += val[i]*(E[i+1]-E[i])/(E0[j+1]-E0[j])
     
     # this adds an additional point in the very beginning
     new_val = np.roll(new_val, 1)
@@ -118,8 +83,8 @@ def plot_serpent_coarse_spectrum():
     This function 
     '''
 
-    data = st.read('oecd-fullcore26G-600_det1b1.m', reader='det')
-    save = 'serpent-fullcore-coarse-spectrum2'
+    data = st.read('oecd-fullcore26G-1200_det1b1.m', reader='det')
+    save = 'serpent-fullcore-1200-coarse-spectrum'
     det = data.detectors['EnergyDetector']
     val = det.tallies
 
@@ -130,38 +95,29 @@ def plot_serpent_coarse_spectrum():
     dE[-1] = Emax - E[-1]
 
     # Integral is normalized to 1
-    inte = sum(val*dE)
-    val = val/inte
+    # inte = sum(val*dE)
+    # val = val/inte
+    val /= sum(val)
 
     plt.figure()
-    plt.step(E, val)
+    # plt.step(E, val)
 
     # 15 groups
     E15d = [1.49e+7, 3.68e+6, 1.11e+5, 7.485e+2, 1.301e+2,
             2.9e+1, 8.32e0, 2.38e0, 6.5e-1, 3.5e-1,
             2.0e-1, 8.0e-2, 5.0e-2, 2.0e-2, 1.0e-2,
             1.0e-5]
+
     E15d.reverse()
     E15d = np.array(E15d)/1e6  # eV to MeV
-
     E1, val1 = reagroup(E, val, E15d)
-    plt.step(E1, val1)
 
+    plt.step(E1, val1)
     plt.xscale('log')
     plt.xlabel('E [MeV]')
-    plt.ylabel(r'Normalized flux [$\frac{n}{cm^2s}$]')
+    plt.ylabel(r'Energy integrated flux [$\frac{n}{cm^2s}$]')
     plt.grid(True)
     plt.savefig(save, dpi=300, bbox_inches="tight")
-    plt.close()
-
-    plt.figure()
-    E2, val2 = reagroup2(E, val, E15d)
-    plt.step(E2, val2)
-    plt.xscale('log')
-    plt.xlabel('E [MeV]')
-    plt.ylabel(r'Normalized flux [$\frac{n}{cm^2s}$]')
-    plt.grid(True)
-    plt.savefig('serpent-fullcore-coarse-spectrum3', dpi=300, bbox_inches="tight")
     plt.close()
 
 
