5869c69a42b9550f158bd86cb6f5c25facab6ed3
robfairh <ref3@illinois.edu>
2020-11-16 08:09:37 -0600

    added functions to plot flux

--- moltres/neutronics/auxiliary.py
+++ moltres/neutronics/auxiliary.py
@@ -10,6 +10,7 @@ def moltres_assembly_legend():
     Adds legend to figure 'preliminary-study/3D-assembly-30deg-reflec-mesh'.
 
     '''
+
     red = mpatches.Patch(color=(1., 0., 0.), label='Fuel')
     green = mpatches.Patch(color=(0, 1., 0.), label='Coolant')
     blue = mpatches.Patch(color=(0., 0., 1.), label='Moderator')
@@ -27,4 +28,77 @@ def moltres_assembly_legend():
                 dpi=300, bbox_inches="tight")
 
 
-moltres_assembly_legend()
+def plotcsv_frommoose_groups(file, save, G=2, dire='z'):
+    '''
+    Moltres output is a csv file.
+    This function plots the values of the multi-group flux in the csv.
+
+    Parameters:
+    -----------
+    file: [string]
+        name of the .csv file
+    save: [string]
+        name of the figure to produce
+    G: [int]
+        number of energy groups
+    dire: [char]
+        direction on which the detector is applied
+        values: 'x', 'y', 'z', 'r'
+    '''
+
+    file = pd.read_csv(file)
+    plt.figure()
+
+    if dire == 'r':
+        x1 = np.array(file['x'].tolist())
+        y1 = np.array(file['y'].tolist())
+        r = np.sqrt(x1**2 + y1**2)
+        x = r
+    else:
+        # x, y, z direction
+        x = file[dire].tolist()
+
+    group1 = file['group1'].tolist()
+    N = len(group1)
+    group = np.zeros((G, N))
+
+    for i in range(G):
+        group[i] = np.array(file['group'+str(i+1)].tolist())
+
+    # If values are unsorted
+    for i in range(G):
+        group[i] = [X for _, X in sorted(zip(x, group[i]))]
+    x.sort()
+
+    # To normalize to the max value of group 1 flux
+    # M = max(group1)
+    # group /= M
+
+    for i in range(G):
+        plt.plot(x, group[i], label='g='+str(i+1))
+
+    if G < 20:
+        plt.legend(loc="upper left", bbox_to_anchor=(1., 1.), fancybox=True)
+    else:
+        # 1.1 or 1.2
+        plt.legend(loc="upper left", bbox_to_anchor=(1., 1.1), fancybox=True)
+
+    plt.xlabel(dire+' [cm]')
+    plt.ylabel(r'$\phi \left[\frac{n}{cm^2s}\right]$')
+    plt.savefig(save, dpi=300, bbox_inches="tight")
+    plt.close()
+
+
+
+def main():
+    # Adds legend to geometry figure
+    moltres_assembly_legend()
+
+    # Plot flux
+    file = 'preliminary-study/3D-assembly-homo-eig-fuel_0002.csv'
+    save = 'preliminary-study/standard-column-homo'
+    plotcsv_frommoose_groups(file, save, G=2, dire='z')
+
+
+if __name__ == "__main__":
+    main()
