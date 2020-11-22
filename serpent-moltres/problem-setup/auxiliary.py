c0cc4aed447a9e7ff32dbf25c65f0c03eca830c2
robfairh <ref3@illinois.edu>
2020-11-22 09:35:53 -0600

    added length scale to images

--- serpent-moltres/problem-setup/auxiliary.py
+++ serpent-moltres/problem-setup/auxiliary.py
@@ -3,6 +3,50 @@ import numpy as np
 from matplotlib.cbook import get_sample_data
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+# import matplotlib.axes.Axes 
+import pandas as pd
+
+
+def standard():
+    '''
+    Adds legend to standard fuel assembly geometry image:
+    'standard-column_geom1.png'.
+    '''
+
+    matrix = mpatches.Patch(color=(0.63, 0.63, 0.63),
+                            label='Graphite Compact Matrix')
+    block = mpatches.Patch(color=(0.61, 1., 0.91), label='Graphite Fuel Block')
+    helium = mpatches.Patch(color=(0.59, 0.41, 1.), label='Helium')
+
+    cwd = os.getcwd()
+    fname = get_sample_data('%s/standard-column_geom1.png' % (cwd))
+    image = plt.imread(fname)
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+
+    xlength = np.round(36/np.cos(np.pi/6), 2)
+    scalex = xlength/1000
+    ticks_x = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x*scalex))
+    ax.xaxis.set_major_formatter(ticks_x)
+    xticks = np.arange(0, np.floor(xlength), 5)/scalex
+    ax.set_xticks(xticks)
+    ax.tick_params(axis="x", labelsize=12)
+
+    ylength = 36.
+    scaley = ylength/1000
+    ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x*scaley))
+    ax.yaxis.set_major_formatter(ticks_y)
+    yticks = np.arange(0, np.floor(ylength), 5)/scaley
+    ax.set_yticks(yticks)
+    ax.tick_params(axis="y", labelsize=12)
+
+    # plt.gca().invert_yaxis()
+    ax.set_xlabel('x [cm]', fontsize=12)
+    ax.set_ylabel('y [cm]', fontsize=12)
+    plt.legend(handles=[matrix, block, helium], loc="lower right",
+               bbox_to_anchor=(1.0, 1.0), fancybox=True, fontsize=12)
+    plt.savefig("standard", dpi=300, bbox_inches="tight")
 
 
 def moltres_assembly_legend():
@@ -11,19 +55,40 @@ def moltres_assembly_legend():
 
     '''
 
-    red = mpatches.Patch(color=(1., 0., 0.), label='Fuel')
+    red = mpatches.Patch(color=(1., 0., 0.), label='Fuel Compact')
     green = mpatches.Patch(color=(0, 1., 0.), label='Coolant')
     blue = mpatches.Patch(color=(0., 0., 1.), label='Moderator')
 
-    figure = 'preliminary-study/3D-assembly-30deg-reflec-mesh'
+    figure = '3D-assembly-30deg-reflec-mesh'
     cwd = os.getcwd()
     fname = get_sample_data('{0}/{1}.png'.format(cwd, figure))
-    im = plt.imread(fname)
-    plt.imshow(im)
-    plt.legend(handles=[red, green, blue],
-               loc="upper right", bbox_to_anchor=(1.0, 0.0), fancybox=True)
+    image = plt.imread(fname)
+    fig, ax = plt.subplots()
+    ax.imshow(image)
 
-    plt.axis('off')
+    xlength = np.round(18/2/np.cos(np.pi/6), 2)
+    scalex = xlength/408
+    ticks_x = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x*scalex))
+    ax.xaxis.set_major_formatter(ticks_x)
+    # xticks = np.array([0, 0.5, 0.9, 1.0])/scalex
+    xticks = np.arange(0, np.floor(xlength), 3)/scalex
+    ax.set_xticks(xticks)
+    ax.tick_params(axis="x", labelsize=12)
+
+    ylength = 18.
+    scaley = ylength/705
+    ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x*scaley))
+    ax.yaxis.set_major_formatter(ticks_y)
+    yticks = np.arange(0, np.floor(ylength), 3)/scaley
+    ax.set_yticks(yticks)
+    ax.tick_params(axis="y", labelsize=12)
+
+    # plt.gca().invert_yaxis()
+    ax.set_xlabel('x [cm]', fontsize=12)
+    ax.set_ylabel('y [cm]', fontsize=12)
+    plt.legend(handles=[red, green, blue], loc="lower right",
+               bbox_to_anchor=(1.0, 1.0), fancybox=True, fontsize=12)
+
     plt.savefig('3D-assembly-30deg-reflec-meshB2',
                 dpi=300, bbox_inches="tight")
 
@@ -91,12 +156,13 @@ def plotcsv_frommoose_groups(file, save, G=2, dire='z'):
 
 def main():
     # Adds legend to geometry figure
+    standard()
     moltres_assembly_legend()
 
     # Plot flux
-    file = '3D-assembly-homo-eig-fuel_0002.csv'
-    save = 'standard-column-homo'
-    plotcsv_frommoose_groups(file, save, G=2, dire='z')
+    # file = '3D-assembly-homo-eig-fuel_0002.csv'
+    # save = 'standard-column-homo'
+    # plotcsv_frommoose_groups(file, save, G=2, dire='z')
 
 
 if __name__ == "__main__":
