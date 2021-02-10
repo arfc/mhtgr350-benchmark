0f25fd7b751dc63803daced1d5c1a22b5d19ec04
robfairh <ref3@illinois.edu>
2021-02-10 19:42:01 -0300

    fixed 2Dgeo.py

--- benchmark-ph1ex1/2D-model/2Dgeo.py
+++ benchmark-ph1ex1/2D-model/2Dgeo.py
@@ -1,21 +1,24 @@
-'''
-This script calculates the equivalent 2D geometry of the full core.
-
-Rm[cm] = point in the middle of the distance between Ro and Ri
-Ri[cm] = inner radius of the active core
-Ro[cm] = outer radius of the active core
-'''
-
 import numpy as np
 
-F = 18/np.cos(np.pi/6)  # cm length of face of the hexagon
-Ah = 6. * (F * 18./2)   # Area of the hexagon
 
-Rm = 6*F
-print('Rm: ', np.round(Rm, 3))
+if __name__ == '__main__':
+    '''
+    This script calculates the equivalent 2D geometry of the full core.
+    It prints out in the terminal the following parameters:
 
-Ri = Rm - Ah * 66 / (4*Rm*np.pi)
-print('Ri: ', np.round(Ri, 3))
+        Ri[cm] = inner radius of the active core
+        Ro[cm] = outer radius of the active core
+        Rm[cm] = point in the middle of the distance between Ro and Ri
+    '''
 
-Ro = 2*Rm-Ri
-print('Ro: ', np.round(Ro, 3))
+    F = 18/np.cos(np.pi/6)  # cm length of face of the hexagon
+    Ah = 6. * (F * 18./2)   # Area of the hexagon
+
+    Rm = 6*F
+    Ri = Rm - Ah * 66 / (4*Rm*np.pi)
+    print('Ri: ', np.round(Ri, 3))
+
+    Ro = 2*Rm-Ri
+    print('Ro: ', np.round(Ro, 3))
+
+    print('Rm: ', np.round(Rm, 3))
